917861313c7297219ffc2e1725b9338878a4de90
ZAXID <ssvladislav2007@gmail.com>
2022-06-07 12:08:48 +0300

    Add main

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
 import telebot
+import random
 from config import bot_token
+
+bot = telebot.TeleBot(bot_token)
+
+DIGITS = [str(x) for x in range(10)]
+my_number = ''
+
+@bot.message_handler(commands=['start', 'game'])
+def start_game(message):
+    digits = DIGITS.copy()
+    my_number = ''
+    for pos in range(4):
+        if pos:
+            digit = random.choice(digits)
+        else: 
+            digit = random.choice(digits[1:])
+        my_number += digit
+        digits.remove(digit)
+    bot.reply_to(message, 
+        f'Я загадал 4-значное число. Попробуй отгадать, {message.from_user.first_name}!')
+
+@bot.message_handler(content_types=['text'])
+def bot_answer(message):
+    text = message.text
+    if len(text) == 4 and text.isnumeric():
+        response = text
+    else:
+        response = 'Пришли мне 4-значное число!'
+    bot.send_message(message.from_user.id, response)
+
+if __name__ == '__main__':
+    bot.polling(non_stop=True)
